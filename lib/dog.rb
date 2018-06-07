8cd11275296ccca4d758cb082d1819e549de00c3
keylimotino <stephanie48758@gmail.com>
2018-06-07 19:39:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,21 +2,11 @@
 
 class Dog
 
-  def initialize(bark)
-    @bark = bark
-  end
-
   def bark
-    @bark
     puts "Woof!"
   end
 
-  def sit (sit)
-    @sit = sit
-  end
-
   def sit
-    @sit
     puts "The Dog is sitting"
   end
 
